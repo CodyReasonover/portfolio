1f1f7893ab04160a0bdb4fc31a44f3f3d3ad4358
Cody Reasonover <120606140+CodyReasonover@users.noreply.github.com>
2022-12-14 17:58:26 -0500

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,27 +1,25 @@
-package cmsc256;
-
 import java.util.Scanner;
 
 public class BankAccount {
-    private double balance;
+    private double balance; // The current balance of the bank account
     public BankAccount() {
-        this.balance = 0.0;
+        this.balance = 0.0; // Initialize the balance to 0.0
     }
 
     public void deposit(double amount) {
-        this.balance += amount;
+        this.balance += amount; // Add the amount to the current balance
     }
 
     public void withdraw(double amount) {
-        if (amount > this.balance) {
-            System.out.println("Insufficient funds");
+        if (amount > this.balance) { // If the withdrawal amount is more than the balance
+            System.out.println("Insufficient funds"); // Print a message indicating insufficient funds
         } else {
-            this.balance -= amount;
+            this.balance -= amount; // Otherwise, subtract the withdrawal amount from the balance
         }
     }
 
     public double getBalance() {
-        return this.balance;
+        return this.balance; // Return the current balance
     }
 
     public static void main(String[] args) {
@@ -29,30 +27,34 @@ public class BankAccount {
         BankAccount account = new BankAccount();
 
         System.out.println("How much money is currently in your bank account?");
-        account.balance = input.nextDouble();
+        account.balance = input.nextDouble(); // Get the initial balance from the user
 
         System.out.println("How much would you like to withdraw?");
-        double amount = input.nextDouble();
+        double amount = input.nextDouble(); // Get the withdrawal amount from the user
 
-        if (amount > 0) {
-            account.withdraw(amount);
-            System.out.println("Your new balance is: " + account.getBalance());
-        } else if (amount < 0) {
-            System.out.println("Please enter a positive amount to withdraw.");
-        } else {
-            System.out.println("Your balance remains the same: " + account.getBalance());
+        if (amount > 0) { // If the withdrawal amount is positive
+            account.withdraw(amount); // Withdraw the amount from the account
+            System.out.println("Your new balance is: " + account.getBalance()); // Print the new balance
+        } else if (amount < 0) { // If the withdrawal amount is negative
+            System.out.println("Please enter a positive amount to withdraw."); // Print an error message
+        } else { // If the withdrawal amount is 0
+            System.out.println("Your balance remains the same: " + account.getBalance()); // Print the current balance
         }
 
         System.out.println("How much would you like to deposit?");
-        amount = input.nextDouble();
+        amount = input.nextDouble(); // Get the deposit amount from the user
 
-        if (amount > 0) {
-            account.deposit(amount);
-            System.out.println("Your new balance is: " + account.getBalance());
-        } else if (amount < 0) {
-            System.out.println("Please enter a positive amount to deposit.");
-        } else {
-            System.out.println("Your balance remains the same: " + account.getBalance());
+        if (amount > 0) { // If the deposit amount is positive
+            account.deposit(amount); // Deposit the amount in the account
+            System.out.println("Your new balance is: " + account.getBalance()); // Print the new balance
+        } else if (amount < 0) { // If the deposit amount is negative
+            System.out.println("Please enter a positive amount to deposit."); // Print an error message
+        } else { // If the deposit amount is 0
+            System.out.println("Your balance remains the same: " + account.getBalance()); // Print the current balance
         }
     }
 }
+
+
+
+
